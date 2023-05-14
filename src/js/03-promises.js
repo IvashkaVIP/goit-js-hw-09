ad4f1fe9aad967359fe5a9607caefbc973510d65
IvashkaVIP <ivashka.co@gmail.com>
2023-05-14 13:02:05 +0300

    refs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,38 @@
 import Notiflix from 'notiflix';
 
+const ref = {
+  inputDelay: document.querySelector('[name=delay]'),
+  inputStep: document.querySelector('[name=step]'),
+  inputAmount: document.querySelector('[name=amount]'),
+  formSubmit: document.querySelector('.form'),
+};
+
+// console.log(ref.inputStep);
+// console.log(ref.inputAmount);
+// console.log(ref.formSubmit);
+
+ref.formSubmit.addEventListener('submit', onFormSubmit);
 
 
-const p = createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    //console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    //console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// const p = createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     //console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     //console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
 
-console.log(p);
+function onFormSubmit(evt) {
+   
+   evt.preventDefault();
+   console.log(ref.inputDelay.value);
+   console.log(ref.inputStep.value);
+   console.log(ref.inputAmount.value);
+
+}
+
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
